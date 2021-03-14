47cc6d329d1c388e947562d8494096b54c12c105
Ricardo L <ricardoleitejr99@gmail.com>
2021-03-14 16:50:16 -0300

    refatoracao com class scroll anima

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,11 +1,11 @@
-export default function fetchBitcoin(url, target) {}
-
-fetch(url)
-  .then((response) => response.json())
-  .then((json) => {
-    const btcPreco = document.querySelector(target);
-    btcPreco.innerText = (100 / json.BRL.sell).toFixed(4);
-  })
-  .catch((erro) => {
-    console.log(erro);
-  });
+export default function fetchBitcoin(url, target) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+      const btcPreco = document.querySelector(target);
+      btcPreco.innerText = (100 / json.BRL.sell).toFixed(4);
+    })
+    .catch((erro) => {
+      console.log(erro);
+    });
+}
