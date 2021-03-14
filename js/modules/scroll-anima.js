1190cfadf692895d72aa393552d9eee696e78973
Ricardo L <ricardoleitejr99@gmail.com>
2021-03-14 17:03:11 -0300

    animacao ao scroll refeita

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -5,20 +5,35 @@ export default class ScrollAnima {
     this.animaScroll = this.animaScroll.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - this.windowMetade <= 0;
-      if (isSectionVisible) {
-        section.classList.add('ativo');
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset) - this.windowMetade,
+      };
+    });
+  }
+
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
       } else if (section.classList.contains('ativo')) {
-        section.classList.remove('ativo');
+        item.element.classList.remove('ativo');
       }
     });
   }
 
   init() {
-    this.animaScroll();
-    window.addEventListener('scroll', this.animaScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+    return this;
+  }
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
